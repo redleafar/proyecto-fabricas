fc528d905992bf8bc8c4ce6234b7ec74987f3558
Rafael <raf_berm2@yahoo.com>
2019-10-25 21:35:35 -0500

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,89 +1,92 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import LoginScreen from "./src/screens/LoginScreen";
 
-import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+export default LoginScreen;
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow
+//  */
 
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+// import React from 'react';
+// import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+// import { Marker } from 'react-native-maps';
 
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    height: '100%',
-    width: '100%',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
- });
+// import {
+//   SafeAreaView,
+//   StyleSheet,
+//   ScrollView,
+//   View,
+//   Text,
+//   StatusBar,
+// } from 'react-native';
 
- var marker =
-  {
-    latitude: 4.684335,
-    longitude: -74.165654,
-    title: 'Foo Place1',
-    subtitle: '1234 Foo Drive'
-  };
+// const styles = StyleSheet.create({
+//   container: {
+//     ...StyleSheet.absoluteFillObject,
+//     height: '100%',
+//     width: '100%',
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   map: {
+//     ...StyleSheet.absoluteFillObject,
+//   },
+//  });
 
-  var marker2 =
-  {
-    latitude: 4.684355,
-    longitude: -74.165664,
-    title: 'Foo Place2',
-    subtitle: '1234 Foo Drive'
-  };
+//  var marker =
+//   {
+//     latitude: 4.684335,
+//     longitude: -74.165654,
+//     title: 'Foo Place1',
+//     subtitle: '1234 Foo Drive'
+//   };
 
-  var marker3 =
-  {
-    latitude: 4.684375,
-    longitude: -74.165694,
-    title: 'Foo Place3',
-    subtitle: '1234 Foo Drive'
-  };
+//   var marker2 =
+//   {
+//     latitude: 4.684355,
+//     longitude: -74.165664,
+//     title: 'Foo Place2',
+//     subtitle: '1234 Foo Drive'
+//   };
 
-  var markers = [marker, marker2, marker3];
+//   var marker3 =
+//   {
+//     latitude: 4.684375,
+//     longitude: -74.165694,
+//     title: 'Foo Place3',
+//     subtitle: '1234 Foo Drive'
+//   };
 
-export default () => (
-  <View style={styles.container}>
-    <MapView
-      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-      style={styles.map}
-      region={{
-        latitude: 4.684335,
-        longitude: -74.165654,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }}
-    >
-    {markers.map(marker => (
-      <Marker
-        coordinate={{
-          latitude: marker.latitude,
-          longitude: marker.longitude}}
-        title={marker.title}
-        description={marker.description}
-        key = {marker.title}
-      />
-    ))}
-      <Marker coordinate={{
-            latitude: marker.latitude,
-            longitude: -74.165654}}>
-      </Marker>
-    </MapView>
-  </View>
-);
+//   var markers = [marker, marker2, marker3];
+
+// export default () => (
+//   <View style={styles.container}>
+//     <MapView
+//       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+//       style={styles.map}
+//       region={{
+//         latitude: 4.684335,
+//         longitude: -74.165654,
+//         latitudeDelta: 0.015,
+//         longitudeDelta: 0.0121,
+//       }}
+//     >
+//     {markers.map(marker => (
+//       <Marker
+//         coordinate={{
+//           latitude: marker.latitude,
+//           longitude: marker.longitude}}
+//         title={marker.title}
+//         description={marker.description}
+//         key = {marker.title}
+//       />
+//     ))}
+//       <Marker coordinate={{
+//             latitude: marker.latitude,
+//             longitude: -74.165654}}>
+//       </Marker>
+//     </MapView>
+//   </View>
+// );
---
+++ src/config/strings.ts
@@ -0,0 +1,7 @@
+const strings = {
+    LOGIN: "Log In",
+    EMAIL_PLACEHOLDER: "Email",
+    PASSWORD_PLACEHOLDER: "Password"
+};
+  
+export default strings;
